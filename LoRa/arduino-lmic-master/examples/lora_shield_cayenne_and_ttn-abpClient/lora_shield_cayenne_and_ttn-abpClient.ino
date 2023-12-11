817a37936cb5128121bc23635820fdff99cfbbe3
Yidong Ren <45118395+YDRen001@users.noreply.github.com>
2023-12-10 23:50:10 -0500

    Add files via upload

---
+++ LoRa/arduino-lmic-master/examples/lora_shield_cayenne_and_ttn-abpClient/lora_shield_cayenne_and_ttn-abpClient.ino
@@ -0,0 +1,327 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Thomas Telkamp and Matthijs Kooijman
+ *
+ * Permission is hereby granted, free of charge, to anyone
+ * obtaining a copy of this document and accompanying files,
+ * to do whatever they want with them without any restriction,
+ * including, but not limited to, copying, modification and redistribution.
+ * NO WARRANTY OF ANY KIND IS PROVIDED.
+ *
+ * This example sends a valid LoRaWAN packet with payload "Hello,
+ * world!", using frequency and encryption settings matching those of
+ * the The Things Network.
+ *
+ * This uses ABP (Activation-by-personalisation), where a DevAddr and
+ * Session keys are preconfigured (unlike OTAA, where a DevEUI and
+ * application key is configured, while the DevAddr and session keys are
+ * assigned/generated in the over-the-air-activation procedure).
+ *
+ * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
+ * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
+ * violated by this sketch when left running for longer)!
+ *
+ * To use this sketch, first register your application and device with
+ * the things network, to set or generate a DevAddr, NwkSKey and
+ * AppSKey. Each device should have their own unique values for these
+ * fields.
+ *
+ * Do not forget to define the radio type correctly in config.h.
+ *
+ *******************************************************************************/
+
+#include <dht.h>
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+
+dht DHT;
+#define DHT11_PIN A0
+const int ctl_pin=4; //define the output pin of realy
+const int flame_pin=3;  //define the input pin of flame sensor
+
+// LoRaWAN NwkSKey, network session key
+// This is the default Semtech key, which is used by the early prototype TTN
+// network.
+static const PROGMEM u1_t NWKSKEY[16] = { 0x48, 0x13, 0xF8, 0x05, 0x90, 0x97, 0xA9, 0x4C, 0xA7, 0x95, 0x5E, 0xDD, 0x51, 0x2C, 0x60, 0x8B };
+
+// LoRaWAN AppSKey, application session key
+// This is the default Semtech key, which is used by the early prototype TTN
+// network.
+static const u1_t PROGMEM APPSKEY[16] = { 0x62, 0xCC, 0xF8, 0xB0, 0xD3, 0xA9, 0x2A, 0x0D, 0x35, 0xD7, 0xBA, 0x0B, 0x26, 0x6D, 0x1E, 0xCA };
+
+// LoRaWAN end-device address (DevAddr)
+static const u4_t DEVADDR = 0x260BE402; // <-- Change this address for every node!
+
+// These callbacks are only used in over-the-air activation, so they are
+// left empty here (we cannot leave them out completely unless
+// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
+
+static float temperature,humidity,tem,hum;
+static uint8_t LPP_data[13] = {0x01,0x67,0x00,0x00,0x02,0x68,0x00,0x03,0x01,0x00,0x04,0x00,0x00}; //0xO1,0x02,0x03,0x04 is Data Channel,0x67,0x68,0x01,0x00 is Data Type
+static uint8_t opencml[4]={0x03,0x00,0x64,0xFF},closecml[4]={0x03,0x00,0x00,0xFF}; //the payload of the cayenne or ttn downlink  //0xO1,0x02 is Data Channel,0x67,0x68 is Data Type
+static unsigned int count = 1; 
+
+static osjob_t sendjob;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 12;
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+    .nss = 10,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 9,
+    .dio = {2, 6, 7},
+};
+
+void onEvent (ev_t ev) {
+    Serial.print(os_getTime());
+    Serial.print(": ");
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            Serial.println(F("EV_SCAN_TIMEOUT"));
+            break;
+        case EV_BEACON_FOUND:
+            Serial.println(F("EV_BEACON_FOUND"));
+            break;
+        case EV_BEACON_MISSED:
+            Serial.println(F("EV_BEACON_MISSED"));
+            break;
+        case EV_BEACON_TRACKED:
+            Serial.println(F("EV_BEACON_TRACKED"));
+            break;
+        case EV_JOINING:
+            Serial.println(F("EV_JOINING"));
+            break;
+        case EV_JOINED:
+            Serial.println(F("EV_JOINED"));
+            break;
+        case EV_RFU1:
+            Serial.println(F("EV_RFU1"));
+            break;
+        case EV_JOIN_FAILED:
+            Serial.println(F("EV_JOIN_FAILED"));
+            break;
+        case EV_REJOIN_FAILED:
+            Serial.println(F("EV_REJOIN_FAILED"));
+            break;
+        case EV_TXCOMPLETE:
+            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            if (LMIC.txrxFlags & TXRX_ACK)
+              Serial.println(F("Received ack"));
+            if(LMIC.dataLen>0)
+            {
+              int i,j=0;
+              uint8_t received[4]={0x00,0x00,0x00,0x00};
+               Serial.println("Received :");
+              for(i=9;i<(9+LMIC.dataLen);i++)   //the received buf
+              {
+                Serial.print(LMIC.frame[i],HEX);
+                received[j]=LMIC.frame[i];
+                j++;
+                Serial.print(" ");
+               }
+              Serial.println(); 
+            if ((received[0]==opencml[0])&&(received[1]==opencml[1])&&(received[2]==opencml[2])&&(received[3]==opencml[3])) {
+              Serial.println("Set pin to HIGH.");
+              digitalWrite(ctl_pin, HIGH);
+            }
+            if ((received[0]==closecml[0])&&(received[1]==closecml[1])&&(received[2]==closecml[2])&&(received[3]==closecml[3])) {
+              Serial.println("Set pin to LOW.");
+               digitalWrite(ctl_pin, LOW);
+            }
+            }
+            // Schedule next transmission
+            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+            break;
+        case EV_LOST_TSYNC:
+            Serial.println(F("EV_LOST_TSYNC"));
+            break;
+        case EV_RESET:
+            Serial.println(F("EV_RESET"));
+            break;
+        case EV_RXCOMPLETE:
+            // data received in ping slot
+            Serial.println(F("EV_RXCOMPLETE"));
+            break;
+        case EV_LINK_DEAD:
+            Serial.println(F("EV_LINK_DEAD"));
+            break;
+        case EV_LINK_ALIVE:
+            Serial.println(F("EV_LINK_ALIVE"));
+            break;
+         default:
+            Serial.println(F("Unknown event"));
+            break;
+    }
+}
+
+void dhtTem()
+{
+       int16_t tem_LPP;
+       temperature = DHT.read11(DHT11_PIN);    //Read Tmperature data
+       tem = DHT.temperature*1.0;      
+       humidity = DHT.read11(DHT11_PIN);      //Read humidity data
+       hum = DHT.humidity* 1.0; 
+       Serial.print("###########    ");
+       Serial.print("COUNT=");
+       Serial.print(count);
+       Serial.println("    ###########");            
+       Serial.println(F("The temperature and humidity:"));
+       Serial.print("[");
+       Serial.print(tem);
+       Serial.print("℃");
+       Serial.print(",");
+       Serial.print(hum);
+       Serial.print("%");
+       Serial.print("]");
+       Serial.println("");
+       count++;
+       tem_LPP=tem * 10; 
+       LPP_data[2] = tem_LPP>>8;
+       LPP_data[3] = tem_LPP;
+       LPP_data[6] = hum * 2;
+}
+
+void pinread()
+{  
+    int val,val1;
+    val=digitalRead(ctl_pin);
+    val1=digitalRead(flame_pin);
+    if(val==1)
+     {
+        LPP_data[9]=0x01;
+     }
+    else
+    {
+        LPP_data[9]=0x00;
+    }
+    if(val1==1)
+    {
+      LPP_data[12]=0x01;
+    }
+    else
+    {
+      LPP_data[12]=0x00;
+    }
+}
+
+void do_send(osjob_t* j){
+    // Check if there is not a current TX/RX job running
+    if (LMIC.opmode & OP_TXRXPEND) {
+        Serial.println(F("OP_TXRXPEND, not sending"));
+    } else {
+        dhtTem();
+        pinread();
+        // Prepare upstream data transmission at the next possible time.
+        LMIC_setTxData2(1,LPP_data, sizeof(LPP_data), 0);
+        Serial.println(F("Packet queued"));
+    }
+    // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void setup() {
+    Serial.begin(9600);
+    while(!Serial);
+    Serial.println("Connect to TTN and Send data to mydevice cayenne(Use DHT11 Sensor):");
+
+    pinMode(ctl_pin,OUTPUT);
+    pinMode(flame_pin,INPUT);
+//    attachInterrupt(1,fire,LOW);  //no connect Flame sensor should commented this code
+    
+    #ifdef VCC_ENABLE
+    // For Pinoccio Scout boards
+    pinMode(VCC_ENABLE, OUTPUT);
+    digitalWrite(VCC_ENABLE, HIGH);
+    delay(1000);
+    #endif
+
+    // LMIC init
+    os_init();
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
+
+    // Set static session parameters. Instead of dynamically establishing a session
+    // by joining the network, precomputed session parameters are be provided.
+    #ifdef PROGMEM
+    // On AVR, these values are stored in flash and only copied to RAM
+    // once. Copy them to a temporary buffer here, LMIC_setSession will
+    // copy them into a buffer of its own again.
+    uint8_t appskey[sizeof(APPSKEY)];
+    uint8_t nwkskey[sizeof(NWKSKEY)];
+    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+    LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
+    #else
+    // If not running an AVR with PROGMEM, just use the arrays directly
+    LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+    #endif
+
+    #if defined(CFG_eu868)
+    // Set up the channels used by the Things Network, which corresponds
+    // to the defaults of most gateways. Without this, only three base
+    // channels from the LoRaWAN specification are used, which certainly
+    // works, so it is good for debugging, but can overload those
+    // frequencies, so be sure to configure the full frequency range of
+    // your network here (unless your network autoconfigures them).
+    // Setting up channels should happen after LMIC_setSession, as that
+    // configures the minimal channel set.
+    // NA-US channels 0-71 are configured automatically
+    //LMIC_setupChannel(0, 866585000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    //LMIC_setupChannel(1, 865402500, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
+    //LMIC_setupChannel(2, 865602500, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    //LMIC_setupChannel(3, 865985000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    //LMIC_setupChannel(4, 866185000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    //LMIC_setupChannel(5, 866385000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    //LMIC_setupChannel(6, 866585000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    //LMIC_setupChannel(7, 866785000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    //LMIC_setupChannel(8, 865700000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
+    LMIC_setupChannel(0, 433175000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
+    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
+    // TTN defines an additional channel at 869.525Mhz using SF9 for class B
+    // devices' ping slots. LMIC does not have an easy way to define set this
+    // frequency and support for class B is spotty and untested, so this
+    // frequency is not configured here.
+    #elif defined(CFG_us915)
+    // NA-US channels 0-71 are configured automatically
+    // but only one group of 8 should (a subband) should be active
+    // TTN recommends the second sub band, 1 in a zero based count.
+    // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
+    LMIC_selectSubBand(1);
+    #endif
+
+    // Disable link check validation
+    LMIC_setLinkCheckMode(0);
+
+    // TTN uses SF9 for its RX2 window.
+    LMIC.dn2Dr = DR_SF12;
+
+    // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
+    LMIC_setDrTxpow(DR_SF7,20);
+
+    // Start job
+    do_send(&sendjob);
+}
+
+void fire()
+{
+     LPP_data[12]=0x00;
+     dhtTem();
+     LMIC_setTxData2(1,LPP_data, sizeof(LPP_data), 0);
+     Serial.println("Have fire,the temperature is send");
+}
+
+void loop() {
+    os_runloop_once();
+}
---
+++ LoRa/arduino-lmic-master/examples/lora_shield_cayenne_and_ttn-otaaClient/lora_shield_cayenne_and_ttn-otaaClient.ino
@@ -0,0 +1,265 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Thomas Telkamp and Matthijs Kooijman
+ *
+ * Permission is hereby granted, free of charge, to anyone
+ * obtaining a copy of this document and accompanying files,
+ * to do whatever they want with them without any restriction,
+ * including, but not limited to, copying, modification and redistribution.
+ * NO WARRANTY OF ANY KIND IS PROVIDED.
+ *
+ * This example sends a valid LoRaWAN packet with payload "Hello,
+ * world!", using frequency and encryption settings matching those of
+ * the The Things Network.
+ *
+ * This uses OTAA (Over-the-air activation), where where a DevEUI and
+ * application key is configured, which are used in an over-the-air
+ * activation procedure where a DevAddr and session keys are
+ * assigned/generated for use with all further communication.
+ *
+ * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
+ * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
+ * violated by this sketch when left running for longer)!
+
+ * To use this sketch, first register your application and device with
+ * the things network, to set or generate an AppEUI, DevEUI and AppKey.
+ * Multiple devices can use the same AppEUI, but each device has its own
+ * DevEUI and AppKey.
+ *
+ * Do not forget to define the radio type correctly in config.h.
+ *
+ *******************************************************************************/
+
+#include <dht.h>
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+
+dht DHT;
+#define DHT11_PIN A0
+const int ctl_pin=4; //define the output pin of realy
+const int flame_pin=3;  //define the input pin of flame sensor
+
+// This EUI must be in little-endian format, so least-significant-byte
+// first. When copying an EUI from ttnctl output, this means to reverse
+// the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
+// 0x70.
+static const u1_t PROGMEM APPEUI[8]={ 0xEF, 0x4C, 0xA0, 0x10, 0x68, 0x7A, 0x46, 0x38 };
+void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
+
+// This should also be in little endian format, see above.
+static const u1_t PROGMEM DEVEUI[8]={ 0xC2, 0xB1, 0x04, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
+void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
+
+// This key should be in big endian format (or, since it is not really a
+// number but a block of memory, endianness does not really apply). In
+// practice, a key taken from ttnctl can be copied as-is.
+// The key shown here is the semtech default key.
+static const u1_t PROGMEM APPKEY[16] ={ 0xA3, 0x97, 0x5D, 0xF9, 0x3F, 0x95, 0x5D, 0x96, 0x42, 0x44, 0xCC, 0x5B, 0x73, 0xD2, 0xA3, 0xB1 };
+void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
+
+static float temperature,humidity,tem,hum;
+static uint8_t LPP_data[13] = {0x01,0x67,0x00,0x00,0x02,0x68,0x00,0x03,0x01,0x00,0x04,0x00,0x00}; //0xO1,0x02,0x03,0x04 is Data Channel,0x67,0x68,0x01,0x00 is Data Type
+static uint8_t opencml[4]={0x03,0x00,0x64,0xFF},closecml[4]={0x03,0x00,0x00,0xFF}; //the payload of the cayenne or ttn downlink 
+static unsigned int count = 1; 
+
+static osjob_t sendjob;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 12;
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+    .nss = 10,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 9,
+    .dio = {2, 6, 7},
+};
+
+void onEvent (ev_t ev) {
+    Serial.print(os_getTime());
+    Serial.print(": ");
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            Serial.println(F("EV_SCAN_TIMEOUT"));
+            break;
+        case EV_BEACON_FOUND:
+            Serial.println(F("EV_BEACON_FOUND"));
+            break;
+        case EV_BEACON_MISSED:
+            Serial.println(F("EV_BEACON_MISSED"));
+            break;
+        case EV_BEACON_TRACKED:
+            Serial.println(F("EV_BEACON_TRACKED"));
+            break;
+        case EV_JOINING:
+            Serial.println(F("EV_JOINING"));
+            break;
+        case EV_JOINED:
+            Serial.println(F("EV_JOINED"));
+
+            // Disable link check validation (automatically enabled
+            // during join, but not supported by TTN at this time).
+            LMIC_setLinkCheckMode(0);
+            break;
+        case EV_RFU1:
+            Serial.println(F("EV_RFU1"));
+            break;
+        case EV_JOIN_FAILED:
+            Serial.println(F("EV_JOIN_FAILED"));
+            break;
+        case EV_REJOIN_FAILED:
+            Serial.println(F("EV_REJOIN_FAILED"));
+            break;
+            break;
+        case EV_TXCOMPLETE:
+            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            if (LMIC.txrxFlags & TXRX_ACK)
+              Serial.println(F("Received ack"));
+           if(LMIC.dataLen>0)
+            {
+              int i,j=0;
+              uint8_t received[4]={0x00,0x00,0x00,0x00};
+               Serial.println("Received :");
+              for(i=9;i<(9+LMIC.dataLen);i++)   //the received buf
+              {
+                Serial.print(LMIC.frame[i],HEX);
+                received[j]=LMIC.frame[i];
+                j++;
+                Serial.print(" ");
+               }
+              Serial.println(); 
+            if ((received[0]==opencml[0])&&(received[1]==opencml[1])&&(received[2]==opencml[2])&&(received[3]==opencml[3])) {
+              Serial.println("Set pin to HIGH.");
+              digitalWrite(ctl_pin, HIGH);
+            }
+            if ((received[0]==closecml[0])&&(received[1]==closecml[1])&&(received[2]==closecml[2])&&(received[3]==closecml[3])) {
+              Serial.println("Set pin to LOW.");
+               digitalWrite(ctl_pin, LOW);
+            }
+            }
+            // Schedule next transmission
+            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+            break;
+        case EV_LOST_TSYNC:
+            Serial.println(F("EV_LOST_TSYNC"));
+            break;
+        case EV_RESET:
+            Serial.println(F("EV_RESET"));
+            break;
+        case EV_RXCOMPLETE:
+            // data received in ping slot
+            Serial.println(F("EV_RXCOMPLETE"));
+            break;
+        case EV_LINK_DEAD:
+            Serial.println(F("EV_LINK_DEAD"));
+            break;
+        case EV_LINK_ALIVE:
+            Serial.println(F("EV_LINK_ALIVE"));
+            break;
+         default:
+            Serial.println(F("Unknown event"));
+            break;
+    }
+}
+
+void dhtTem()
+{
+       int16_t tem_LPP;
+       temperature = DHT.read11(DHT11_PIN);    //Read Tmperature data
+       tem = DHT.temperature*1.0;      
+       humidity = DHT.read11(DHT11_PIN);      //Read humidity data
+       hum = DHT.humidity* 1.0; 
+       Serial.print("###########    ");
+       Serial.print("COUNT=");
+       Serial.print(count);
+       Serial.println("    ###########");            
+       Serial.println(F("The temperature and humidity:"));
+       Serial.print("[");
+       Serial.print(tem);
+       Serial.print("℃");
+       Serial.print(",");
+       Serial.print(hum);
+       Serial.print("%");
+       Serial.print("]");
+       Serial.println("");
+       count++;
+       tem_LPP=tem * 10; 
+       LPP_data[2] = tem_LPP>>8;
+       LPP_data[3] = tem_LPP;
+       LPP_data[6] = hum * 2;
+}
+
+void pinread()
+{  
+    int val,val1;
+    val=digitalRead(ctl_pin);
+    val1=digitalRead(flame_pin);
+    if(val==1)
+     {
+        LPP_data[9]=0x01;
+     }
+    else
+    {
+        LPP_data[9]=0x00;
+    }
+    if(val1==1)
+    {
+      LPP_data[12]=0x01;
+    }
+    else
+    {
+      LPP_data[12]=0x00;
+    }
+}
+
+void do_send(osjob_t* j){
+    // Check if there is not a current TX/RX job running
+    if (LMIC.opmode & OP_TXRXPEND) {
+        Serial.println(F("OP_TXRXPEND, not sending"));
+    } else {
+        dhtTem();
+        pinread();
+        // Prepare upstream data transmission at the next possible time.
+        LMIC_setTxData2(1,LPP_data, sizeof(LPP_data), 0);
+        Serial.println(F("Packet queued"));
+    }
+    // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void setup() {
+    Serial.begin(9600);
+    while(!Serial);
+    Serial.println("Connect to TTN and Send data to mydevice cayenne(Use DHT11 Sensor):");
+
+    pinMode(ctl_pin,OUTPUT);
+    pinMode(flame_pin,INPUT);
+//  attachInterrupt(1,fire,LOW);  //no connect Flame sensor should commented this code
+    
+    #ifdef VCC_ENABLE
+    // For Pinoccio Scout boards
+    pinMode(VCC_ENABLE, OUTPUT);
+    digitalWrite(VCC_ENABLE, HIGH);
+    delay(1000);
+    #endif
+
+    // LMIC init
+    os_init();
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
+
+    // Start job (sending automatically starts OTAA too)
+    do_send(&sendjob);
+}
+
+void fire()
+{
+     LPP_data[12]=0x00;
+     dhtTem();
+     LMIC_setTxData2(1,LPP_data, sizeof(LPP_data), 0);
+     Serial.println("Have fire,the temperature is send");
+}
+
+void loop() {
+     os_runloop_once();
+}
